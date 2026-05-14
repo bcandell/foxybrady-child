dcc5f92c3c037e80443f1a2990724223595f08f5
bcandell <bcandell@gmail.com>
2026-05-14 10:08:32 -0400

    Fix child theme font enqueue

--- functions.php
+++ functions.php
@@ -3,18 +3,17 @@
 add_action('wp_enqueue_scripts', function () {
 
     wp_enqueue_style(
+        'foxy-fonts',
+        'https://fonts.googleapis.com/css2?family=Bree+Serif&family=Manrope:wght@200..800&family=Oswald:wght@200..700&display=swap',
+        [],
+        null
+    );
+
+    wp_enqueue_style(
         'foxybrady-child',
         get_stylesheet_uri(),
-        ['generate-style'],
+        ['generate-style', 'foxy-fonts'],
         wp_get_theme()->get('Version')
     );
 
 });
-
-
-wp_enqueue_style(
-    'foxy-fonts',
-    'https://fonts.googleapis.com/css2?family=Bree+Serif&family=Manrope:wght@200..800&family=Oswald:wght@200..700&display=swap',
-    [],
-    null
-);
