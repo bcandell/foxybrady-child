68b3059085707e5b08e603928441bfad9a7477a5
bcandell <bcandell@gmail.com>
2026-05-14 10:37:47 -0400

    simplify home page

--- page-home.php
+++ page-home.php
@@ -12,44 +12,14 @@ $img_overlay  = get_field('img_overlay');
 ?>
 
 <section class="hero">
-
-  <?php if ( $bg_video_mp4 || $bg_video_webm ) : ?>
-
-    <video
-      class="hero__video"
-      autoplay
-      muted
-      loop
-      playsinline
-      poster="<?php echo esc_url($bg_img['url']); ?>"
-    >
-
-
-        <source
-          src="<?php echo esc_url($bg_video_webm['url']); ?>"
-          type="video/webm"
-        >
-
-        <source
-          src="<?php echo esc_url($bg_video_mp4['url']); ?>"
-          type="video/mp4"
-        >
-
-
+    <video class="hero__video" autoplay muted loop playsinline poster="<?php echo esc_url($bg_img['url']); ?>">
+        <source src="<?php echo esc_url($bg_video_webm['url']); ?>" type="video/webm">
+        <source src="<?php echo esc_url($bg_video_mp4['url']); ?>" type="video/mp4">
     </video>
-
-
-
   <div class="hero__overlay"></div>
-
   <div class="hero__content">
-
     <?php if ( $img_overlay ) : ?>
-      <img
-        class="hero__logo"
-        src="<?php echo esc_url($img_overlay['url']); ?>"
-        alt="<?php echo esc_attr($img_overlay['alt']); ?>"
-      >
+      <img class="hero__logo" src="<?php echo esc_url($img_overlay['url']); ?>" alt="<?php echo esc_attr($img_overlay['alt']); ?>">
     <?php endif; ?>
 
     <h1>Live Performances. Captured Cinematically.</h1>
